163a2e345a61eb384f1068035a3873ef2f156b41
ziji <ziji@DESKTOP-QSO21IB>
2019-04-03 20:17:29 +0800

    添加项目文件。

---
+++ WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Tip tip;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            tip = new Tip();
+        }
+
+        private void BillAmountTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BillAmountTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+            performCalculation();
+        }
+
+        private void RadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            performCalculation();
+        }
+
+        private void BillAmountTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void performCalculation()
+        {
+            var selectedRadio = StackPanel.Children.OfType<RadioButton>().FirstOrDefault(r => r.IsChecked == true);
+            if (selectedRadio != null)
+            {
+                tip.CalculateTip(billAmountTextBox.Text, double.Parse(selectedRadio.Tag.ToString()));
+                amountToTipTextBlock.Text = tip.TipAmount;
+                amountToTotalTextBlock.Text = tip.TotalAmount;
+            }
+        }
+    }
+}
+    
+
+
